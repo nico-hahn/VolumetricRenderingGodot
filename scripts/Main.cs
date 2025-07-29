d46bde60eb3c28d15558d9d21e44db3aecaa5db2
Nico Hahn <nico.hahn@zeiss.com>
2025-07-29 08:48:21 +0200

    setup xr project, incl. passthrough

---
+++ scripts/Main.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+namespace VolumeRendering.scripts;
+
+public partial class Main : Node3D
+{
+	private XRInterface _xrInterface;
+
+	public override void _Ready()
+	{
+		_xrInterface = XRServer.FindInterface("OpenXR");
+		if (_xrInterface != null && _xrInterface.IsInitialized())
+		{
+			GD.Print("OpenXR is initialized");
+
+			// Turn off v-sync!
+			DisplayServer.WindowSetVsyncMode(DisplayServer.VSyncMode.Disabled);
+
+			// Change our main viewport to output to the HMD
+			GetViewport().UseXR = true;
+
+			// Must be set to use passthrough
+			_xrInterface.EnvironmentBlendMode = XRInterface.EnvironmentBlendModeEnum.AlphaBlend;
+		}
+		else
+		{
+			GD.Print("OpenXR not initialized, please check if your headset is connected");
+		}
+	}
+}
